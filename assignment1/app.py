54b61939585c7aabfd0695188c9414c41e094fa5
kishan kumar sahu <sahu.kishan0444@gmail.com>
2025-07-06 19:27:30 +0530

    Initial Files

---
+++ assignment1/app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template ,url_for
+
+app=Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html",title="home")
+@app.route("/about")
+def about():
+    return render_template("about.html",title="about")
+@app.route("/projects")
+def projects():
+    return render_template("projects.html",title="projects")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html",title="contact")
+
+if __name__ == "__main__":
+    app.run(debug=True)
